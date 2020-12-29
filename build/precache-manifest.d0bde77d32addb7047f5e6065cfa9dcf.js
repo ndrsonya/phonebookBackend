04e993ac47c9fd23ac01d3ac91e94dcf2d6d055b
ndrsonya <ViktorAnderson@MBP-Viktor.home>
2020-12-29 15:01:09 +0200

    main page added

---
+++ build/precache-manifest.d0bde77d32addb7047f5e6065cfa9dcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1534d5a0a703f77475d63f0b1660db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dc34bf6521e311abf12",
+    "url": "/static/css/main.ce0b6781.chunk.css"
+  },
+  {
+    "revision": "29d90cdbc36696dfc3c1",
+    "url": "/static/js/2.d26cb849.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d26cb849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc34bf6521e311abf12",
+    "url": "/static/js/main.4477d76c.chunk.js"
+  },
+  {
+    "revision": "194d2a893d70517d6cc2",
+    "url": "/static/js/runtime-main.8f096503.js"
+  }
+]);
