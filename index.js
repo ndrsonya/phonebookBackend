5dee1579b55f1a57b184d3ea86cab3a77b74d64b
ndrsonya <ViktorAnderson@MBP-Viktor.home>
2020-12-29 14:43:31 +0200

    new commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 app.use(express.json())
-
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -108,7 +109,7 @@ app.post('/api/persons', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
